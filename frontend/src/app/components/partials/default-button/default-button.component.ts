37fe20026972150de5d830836b6ce393fa61c2a1
AbrahamMzansie <abraham.nkomo@gmail.com>
2023-08-23 23:55:31 +0200

    created custom button

--- frontend/src/app/components/partials/default-button/default-button.component.ts
+++ frontend/src/app/components/partials/default-button/default-button.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input , Output , EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-default-button',
   templateUrl: './default-button.component.html',
-  styleUrls: ['./default-button.component.css']
+  styleUrls: ['./default-button.component.css'],
 })
 export class DefaultButtonComponent {
+  @Input() type: 'submit' | 'button' = 'submit';
+  @Input() text: string = 'Submit';
+  @Input() bgColor: string = '#e72929';
+  @Input() color: string = 'white';
+  @Input() fontSizeRem: number = 1.3;
+  @Input() widthRem: number = 12;
+  @Output() onClick = new EventEmitter();
 
+  constructor() {}
 }
